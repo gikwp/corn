f16c6f5efcf2c3acac2fd6f149de7c4c8e3727fb
Garegin Hakobyan <garik.28h@gmail.com>
2023-01-14 15:04:43 +0100

    Create corn

---
+++ corn.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Plugin Name:   Corn
+ * Plugin URI:    https://github.com/gikwp/corn
+ * Description:   Lazy-loaded component for WordPress themes WpKit.
+ * Version:       1.x-dev
+ * Author:        GikWp
+ * Author URI:    http://gikwp.com/
+ * License:       MIT
+ * License URI:   http://opensource.org/licenses/MIT
+ */
+
+require_once __DIR__ . '/vendor/autoload.php';
+
+// Gikwp\add_actions(['plugins_loaded', 'rest_api_init'], \Gikwp\bootloader(), 5);
